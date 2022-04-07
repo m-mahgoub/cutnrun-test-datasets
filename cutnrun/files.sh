f7be1b046e0ae9c5556c99dba85fe346c44f00ba
m-mahgoub <mmohamedivr@gmail.com>
2022-04-07 13:00:38 -0400

    rename files and add "_remote"

---
+++ cutnrun/files.sh
@@ -0,0 +1,11 @@
+
+mv SPT5_INPUT_rep1_local.bigwig SPT5_INPUT_rep1_remote.bigwig
+mv SPT5_INPUT_rep2_local.bigwig SPT5_INPUT_rep2_remote.bigwig
+mv SPT5_T0_rep1_local.bed SPT5_T0_rep1_local_remote.bed
+mv SPT5_T0_rep1_local.bigwig SPT5_T0_rep1_remote.bigwig
+mv SPT5_T0_rep2_local.bed SPT5_T0_rep2_local_remote.bed
+mv SPT5_T0_rep2_local.bigwig SPT5_T0_rep2_remote.bigwig
+mv SPT5_T15_rep1_local.bed SPT5_T15_rep1_local_remote.bed
+mv SPT5_T15_rep1_local.bigwig SPT5_T15_rep1_remote.bigwig
+mv SPT5_T15_rep2_local.bed SPT5_T15_rep2_local_remote.bed
+mv SPT5_T15_rep2_local.bigwig SPT5_T15_rep2_remote.bigwig
